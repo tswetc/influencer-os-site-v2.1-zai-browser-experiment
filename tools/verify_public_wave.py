d8edc1ccf53d0b8349d1763b8d2eda18c3a46ce0
tswetc <tswetc1@gmail.com>
2026-09-25 01:31:25 +0200

    privacy: remove retired identity literals from verifier

--- tools/verify_public_wave.py
+++ tools/verify_public_wave.py
@@ -18,8 +18,23 @@ if len(sys.argv)!=2:
 
 repo=Path(sys.argv[1]).expanduser().resolve()
 root=repo/"04-MEDIA/transport/wave01-v4"
+retired_id="founder_"+"milena_"+"ioanna"
+retired_name="Milena"+" "+"Ioanna"
+retired_handle="@"+"milena"+"ioanna"
+retired_domain="milena"+"ioanna.com"
+retired_card="MILENA"+" "+"MILANI"+" "+"CAROL"
 forbidden=re.compile(
-    r"(founder_milena_ioanna|milena\s+ioanna|@milenaioanna|milenaioanna\.com|milena\s+milani\s+carol|/Users/|/Volumes/|ssd_relative_source_path|source_path)",
+    "("+"|".join([
+        re.escape(retired_id),
+        re.escape(retired_name),
+        re.escape(retired_handle),
+        re.escape(retired_domain),
+        re.escape(retired_card),
+        r"/Users/",
+        r"/Volumes/",
+        r"ssd_relative_source_path",
+        r"source_path",
+    ])+")",
     re.I,
 )
 errors=[]
